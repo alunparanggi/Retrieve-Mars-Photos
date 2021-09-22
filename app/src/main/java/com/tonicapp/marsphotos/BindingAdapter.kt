171b04eaae9d93fefff41a0cca9be1b0ad438530
alunparanggi <alun.paranggi@gmail.com>
2021-09-22 17:31:56 +0700

    network status finished

--- app/src/main/java/com/tonicapp/marsphotos/BindingAdapter.kt
+++ app/src/main/java/com/tonicapp/marsphotos/BindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.tonicapp.marsphotos
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
@@ -8,6 +9,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.tonicapp.marsphotos.network.MarsProperty
 import com.tonicapp.marsphotos.ui.overview.MarsPhotoAdapter
+import com.tonicapp.marsphotos.ui.overview.NetworkStatus
 
 @BindingAdapter("listData")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?){
@@ -25,5 +27,22 @@ fun loadImgFromUrl(imgView: ImageView, imgUrl: String?){
                 .error(R.drawable.ic_broken_image)
                 .placeholder(R.drawable.loading_animation))
             .into(imgView)
+    }
+}
+
+@BindingAdapter("marsApiStatus")
+fun setImgByApiStatus(imgView: ImageView, apiStatus: NetworkStatus){
+    when(apiStatus){
+        NetworkStatus.LOADING -> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.loading_animation)
+        }
+        NetworkStatus.ERROR -> {
+            imgView.visibility = View.VISIBLE
+            imgView.setImageResource(R.drawable.ic_connection_error)
+        }
+        NetworkStatus.DONE -> {
+            imgView.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/tonicapp/marsphotos/ui/overview/OverviewViewModel.kt
+++ app/src/main/java/com/tonicapp/marsphotos/ui/overview/OverviewViewModel.kt
@@ -1,6 +1,5 @@
 package com.tonicapp.marsphotos.ui.overview
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,11 +9,17 @@ import com.tonicapp.marsphotos.network.MarsProperty
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
+enum class NetworkStatus { LOADING, DONE, ERROR }
+
 class OverviewViewModel: ViewModel() {
 
     private val _properties = MutableLiveData<List<MarsProperty>>()
     val properties: LiveData<List<MarsProperty>>
         get() = _properties
+
+    private val _status = MutableLiveData<NetworkStatus>()
+    val status: LiveData<NetworkStatus>
+        get() = _status
 
     init {
         getMarsPhoto()
@@ -22,10 +27,13 @@ class OverviewViewModel: ViewModel() {
 
     private fun getMarsPhoto(){
         viewModelScope.launch {
+            _status.value = NetworkStatus.LOADING
             try {
                 _properties.value = MarsApi.retrofitService.getPhotos()
+                _status.value = NetworkStatus.DONE
             } catch (e: Exception) {
                 _properties.value = ArrayList()
+                _status.value = NetworkStatus.ERROR
             }
         }
     }
